6720a67bb97377415409088d23b8770078ed0528
Kolapo Abolarinwa <ask_kol@yahoo.co.uk>
2017-02-21 16:17:41 +0000

    Completed Basic Mvc setup
    Setup the basic for Mvc to work on this website, using Mock repositories where database access was required.

--- ThePieShopGit/Startup.cs
+++ ThePieShopGit/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ThePieShopGit.Models;
 
 namespace ThePieShopGit
 {
@@ -11,7 +12,9 @@ namespace ThePieShopGit
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore();
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
